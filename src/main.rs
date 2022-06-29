44a5b453f4820168a44b024ad69bc41117d1fb3c
Janjanjan1 <janeetbaj@gmail.com>
2022-06-29 09:55:04 +0545

    COMMENTS AND UPDATES

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,25 @@ fn update(
     popn: &Vec<Vec<u8>>,
     HEIGHT: usize,
     WIDTH: usize,
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
     next_value: u8,
 ) -> bool {
     let mut count: u8 = 0;
+    // CIRCULAR COORDIANTES FILTER
     let mut filter: [(i32, i32); 8] = [
-        (0, 1),
-        (1, 0),
-        (0, -1),
-        (-1, 0),
-        (1, 1),
-        (-1, -1),
-        (1, -1),
-        (-1, 1),
+        (0 + x, 1 + y),
+        (1 + x, 0 + y),
+        (0 + x, -1 + y),
+        (-1 + x, 0 + y),
+        (1 + x, 1 + y),
+        (-1 + x, -1 + y),
+        (1 + x, -1 + y),
+        (-1 + x, 1 + y),
     ];
     for i in 0..filter.len() {
-        filter[i].0 += x as i32;
-        filter[i].1 += y as i32;
+        // filter[i].0 += x as i32;
+        // filter[i].1 += y as i32;
         if filter[i].1 > HEIGHT as i32 {
             filter[i].1 = HEIGHT as i32 - filter[i].1;
         }
@@ -37,7 +38,6 @@ fn update(
         if filter[i].0 < 0 {
             filter[i].0 = WIDTH as i32 + filter[i].0;
         }
-        // println!("INDEX {:?}", filter[i]);
         if popn[filter[i].0 as usize][filter[i].1 as usize] == next_value {
             count += 1;
         };
@@ -62,7 +62,9 @@ fn main() {
             pallete[i][j] = rng.gen_range(0..254) as u8;
         }
     }
-    // INIT POPN WITH RANDOM NUMBERS IN RANGE
+    // INIT POPN WITH RANDOM NUMBERS IN RANGE. USING CERTAIN COMBINATIONS OF
+    // PROBABILITIES FOR THE DISTRIBUTION FOR RNG WILL PRODUCE MORE CONSISTENT
+    // SPIRALS WITH LESS DEVOURING PYRAMIDS.
     let mut popn: Vec<Vec<u8>> = vec![];
     for _ in 0..HEIGHT {
         let mut row: Vec<u8> = vec![];
@@ -74,6 +76,8 @@ fn main() {
     let mut next_value: u8;
     let mut all_array: Vec<Vec<Vec<u8>>> = vec![];
     let mut new_popn: Vec<Vec<u8>> = popn.clone();
+    use std::time::Instant;
+    let now = Instant::now();
     for gen in 0..GENS {
         println!("Generation: {}", gen);
         for i in 0..HEIGHT {
@@ -82,7 +86,7 @@ fn main() {
                 if next_value == MAXVAL {
                     next_value = 0;
                 }
-                if update(&popn, h, w, i, j, next_value) {
+                if update(&popn, h, w, i as i32, j as i32, next_value) {
                     new_popn[i][j] = next_value;
                 }
             }
@@ -90,6 +94,12 @@ fn main() {
         popn = new_popn.clone();
         all_array.push(popn.clone());
     }
+    let elapsed = now.elapsed();
+    println!(
+        "Elapsed: {:.2?}. Time Per Iteration {:.2?}",
+        elapsed,
+        elapsed / GENS as u32
+    );
     // WAS TOO LAZY TO FIND A BETTER SOLUTION. ARRAY TO GIF BLOCK USING VEC OF ALL COMPUTED STATES . SUPER SLOW.
     let mut image = File::create("done.gif").unwrap();
     let mut encoder = gif::Encoder::new(&mut image, WIDTH as u16, HEIGHT as u16, &[]).unwrap();
